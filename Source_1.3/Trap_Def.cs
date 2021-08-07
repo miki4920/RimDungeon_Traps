ea477f8c5d44fa2584ff57a05e741106f0fffc6a
Nicolas Grobelny <miki4920@gmail.com>
2021-08-07 17:19:45 +0100

    Removed slows variable from trap, added variables for handling each creature type

--- Source_1.3/Trap_Def.cs
+++ Source_1.3/Trap_Def.cs
@@ -7,25 +7,25 @@ namespace RimDungeon
         public int numberOfAttacks = 5;
         public float armorPenetration = 0.015f;
 
-        public float sameFactionSpringChance = 0.005f;
-        public float wildAnimalSpringChance = 0.2f;
+        public float sameFactionSpringChance = 0f;
+        public float wildAnimalSpringChance = 0f;
 
         public int pathFindCost = 8000;
-        public int pathWalkCost = 50;
+        public int pathWalkCost = 20;
 
         public bool rearmable = false;
-        public bool manualSpring = false;   
-        public GraphicData trapUnarmedGraphicData;
-
-        public bool slows = false;
-
+        public bool manualSpring = false;  
     }
     public class Hediff_Trap_Def : DefModExtension
     {
         public HediffDef hediff = null;
-        public float hediffSeverity = 1.0f;
-        public float hediffChance = 1.0f;
-        public bool insectOnly = false;
+        public float severity = 1.0f;
+        public float chance = 1.0f;
+        public bool toxic = false;
+        public bool affectsAll = false;
+        public bool affectsLiving = false;
+        public bool affectsMech = false;
+        public bool affectsInsect = false;
     }
     public class Gas_Trap_Def : DefModExtension
     {
