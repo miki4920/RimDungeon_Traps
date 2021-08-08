7ea7ba62aa6af4271f2648ce62e9724309eb19a7
Nicolas Grobelny <miki4920@gmail.com>
2021-08-08 18:17:47 +0100

    Added a way to refill poison traps with shells

--- Source_1.3/AI/JobDriver_RearmTrap.cs
+++ Source_1.3/AI/JobDriver_RearmTrap.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Verse;
 using Verse.AI;
+using RimWorld;
 
 namespace RimDungeon
 {
@@ -12,6 +13,24 @@ namespace RimDungeon
         public override bool TryMakePreToilReservations(bool errorOnFailed)
         {
             return this.pawn.Reserve(this.job.targetA, this.job, 1, -1, null);
+        }
+
+        private static bool TrapNeedsLoading(Building b)
+        {
+            Trap_Framework trap = b as Trap_Framework;
+            if (trap == null)
+            {
+                return false;
+            }
+            CompChangeableProjectile compChangeableProjectile = trap.TryGetComp<CompChangeableProjectile>();
+            return compChangeableProjectile != null && !compChangeableProjectile.Loaded;
+        }
+
+        public static Thing FindShellForTrap(Pawn pawn, Trap_Framework trap)
+        {
+            StorageSettings allowedShellsSettings = pawn.IsColonist ? trap.TryGetComp<CompChangeableProjectile>().allowedShellsSettings : null;
+            Predicate<Thing> validator = (Thing t) => !t.IsForbidden(pawn) && pawn.CanReserve(t, 10, 1, null, false) && (allowedShellsSettings == null || allowedShellsSettings.AllowedToAccept(t));
+            return GenClosest.ClosestThingReachable(trap.Position, trap.Map, ThingRequest.ForGroup(ThingRequestGroup.Shell), PathEndMode.OnCell, TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false, false, false), 40f, validator, null, 0, -1, false, RegionType.Set_Passable, false);
         }
 
         protected override IEnumerable<Toil> MakeNewToils()
@@ -25,23 +44,56 @@ namespace RimDungeon
             };
             gotoThing.defaultCompleteMode = ToilCompleteMode.PatherArrival;
             gotoThing.FailOnDespawnedNullOrForbidden(TargetIndex.A);
+            Toil loadIfNeeded = new Toil();
+            loadIfNeeded.initAction = delegate ()
+            {
+                Pawn actor = loadIfNeeded.actor;
+                Building building = (Building)actor.CurJob.targetA.Thing;
+                Trap_Framework trap = building as Trap_Framework;
+                if (!JobDriver_RearmTrap.TrapNeedsLoading(building))
+                {
+                    this.JumpToToil(gotoThing);
+                    return;
+                }
+                Thing thing = JobDriver_RearmTrap.FindShellForTrap(this.pawn, trap);
+                if (thing == null)
+                {
+                    actor.jobs.EndCurrentJob(JobCondition.Incompletable, true, true);
+                }
+                actor.CurJob.targetB = thing;
+                actor.CurJob.count = 1;
+            };
+            yield return loadIfNeeded;
+            yield return Toils_Reserve.Reserve(TargetIndex.B, 10, 1, null);
+            yield return Toils_Goto.GotoThing(TargetIndex.B, PathEndMode.OnCell).FailOnSomeonePhysicallyInteracting(TargetIndex.B);
+            yield return Toils_Haul.StartCarryThing(TargetIndex.B, false, false, false);
+            yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.Touch);
+            Toil loadShell = new Toil();
+            loadShell.initAction = delegate ()
+            {
+                Pawn actor = loadShell.actor;
+                Trap_Framework trap = ((Building)actor.CurJob.targetA.Thing) as Trap_Framework;
+                trap.TryGetComp<CompChangeableProjectile>().LoadShell(actor.CurJob.targetB.Thing.def, 1);
+                actor.carryTracker.innerContainer.ClearAndDestroyContents(DestroyMode.Vanish);
+            };
+            yield return loadShell;
             yield return gotoThing;
             yield return Toils_General.Wait(RearmTicks).WithProgressBarToilDelay(TargetIndex.A, false, -0.5f);
-            yield return new Toil
-            {
-                initAction = delegate ()
-                {
-                    Thing thing = this.job.targetA.Thing;
-                    Designation designation = base.Map.designationManager.DesignationOn(thing, DesignationDefOf.RearmTrap);
-                    if (designation != null)
-                    {
-                        designation.Delete();
-                    }
-                    Trap_Framework trap = thing as Trap_Framework;
-                    trap.Rearm();
-                },
-                defaultCompleteMode = ToilCompleteMode.Instant
-            };
+            Toil rearmTrap = new Toil();
+            rearmTrap.initAction = delegate ()
+               {
+                   Pawn actor = loadIfNeeded.actor;
+                   Building building = (Building)actor.CurJob.targetA.Thing;
+                   Trap_Framework trap = building as Trap_Framework;
+                   Thing thing = this.job.targetA.Thing;
+                   Designation designation = base.Map.designationManager.DesignationOn(thing, DesignationDefOf.RearmTrap);
+                   if (designation != null)
+                   {
+                       designation.Delete();
+                   }
+                   trap.Rearm();
+               };
+            yield return rearmTrap;
             yield break;
         }
 
--- Source_1.3/AI/WorkGiver_RearmTrap.cs
+++ Source_1.3/AI/WorkGiver_RearmTrap.cs
@@ -30,22 +30,33 @@ namespace RimDungeon
             return Danger.Deadly;
         }
 
-        public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
+        public override bool HasJobOnThing(Pawn pawn, Thing trap, bool forced = false)
         {
-            if (pawn.Map.designationManager.DesignationOn(t, DesignationDefOf.RearmTrap) == null)
+            if (pawn.Map.designationManager.DesignationOn(trap, DesignationDefOf.RearmTrap) == null)
             {
                 return false;
             }
-            LocalTargetInfo target = t;
+            LocalTargetInfo target = trap;
             if (!pawn.CanReserve(target, 1, -1, null, forced))
             {
                 return false;
             }
-            List<Thing> thingList = t.Position.GetThingList(t.Map);
+            List<Thing> thingList = trap.Position.GetThingList(trap.Map);
             for (int i = 0; i < thingList.Count; i++)
             {
                 IntVec3 intVec;
-                if (thingList[i] != t && thingList[i].def.category == ThingCategory.Item && (thingList[i].IsForbidden(pawn) || thingList[i].IsInValidStorage() || !HaulAIUtility.CanHaulAside(pawn, thingList[i], out intVec)))
+                if (thingList[i] != trap && thingList[i].def.category == ThingCategory.Item && (thingList[i].IsForbidden(pawn) || thingList[i].IsInValidStorage() || !HaulAIUtility.CanHaulAside(pawn, thingList[i], out intVec)))
+                {
+                    return false;
+                }
+            }
+            CompChangeableProjectile compChangeableProjectile = trap.TryGetComp<CompChangeableProjectile>();
+            if (compChangeableProjectile != null && !compChangeableProjectile.Loaded)
+            {
+                StorageSettings allowedShellsSettings = pawn.IsColonist ? trap.TryGetComp<CompChangeableProjectile>().allowedShellsSettings : null;
+                Predicate<Thing> validator = (Thing t) => !t.IsForbidden(pawn) && pawn.CanReserve(t, 10, 1, null, false) && (allowedShellsSettings == null || allowedShellsSettings.AllowedToAccept(t));
+                Thing shell = GenClosest.ClosestThingReachable(trap.Position, trap.Map, ThingRequest.ForGroup(ThingRequestGroup.Shell), PathEndMode.OnCell, TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false, false, false), 40f, validator, null, 0, -1, false, RegionType.Set_Passable, false);
+                if (shell == null)
                 {
                     return false;
                 }
@@ -53,12 +64,12 @@ namespace RimDungeon
             return true;
         }
 
-        public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
+        public override Job JobOnThing(Pawn pawn, Thing trap, bool forced = false)
         {
-            List<Thing> thingList = t.Position.GetThingList(t.Map);
+            List<Thing> thingList = trap.Position.GetThingList(trap.Map);
             for (int i = 0; i < thingList.Count; i++)
             {
-                if (thingList[i] != t && thingList[i].def.category == ThingCategory.Item)
+                if (thingList[i] != trap && thingList[i].def.category == ThingCategory.Item)
                 {
                     Job job = HaulAIUtility.HaulAsideJobFor(pawn, thingList[i]);
                     if (job != null)
@@ -67,7 +78,7 @@ namespace RimDungeon
                     }
                 }
             }
-            return new Job(JobDefOf.RearmTrapJob, t);
+            return new Job(JobDefOf.RearmTrapJob, trap);
         }
     }
 }
