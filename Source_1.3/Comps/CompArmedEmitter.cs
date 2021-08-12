415c55652c459a1098a7cef3a9b309c17e01d3a8
Nicolas Grobelny <miki4920@gmail.com>
2021-08-12 22:29:00 +0100

    Added an Emitter for generating alert particles upon armed trap

---
+++ Source_1.3/Comps/CompArmedEmitter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+
+namespace RimDungeon
+{
+    class CompArmedEmitter : ThingComp
+    {
+		private CompProperties_ArmedEmitter Props
+		{
+			get
+			{
+				return (CompProperties_ArmedEmitter) this.props;
+			}
+		}
+		public override void CompTick()
+		{
+			Trap_Framework trap = (Trap_Framework) this.parent;
+			if(trap.TrapDef.rearmable && !trap.armed)
+            {
+				return;
+            }
+			if (this.Props.emissionInterval != -1)
+			{
+				if (this.ticksSinceLastEmitted >= this.Props.emissionInterval)
+				{
+					this.Emit();
+					this.ticksSinceLastEmitted = 0;
+					return;
+				}
+				this.ticksSinceLastEmitted++;
+			}
+		}
+		protected void Emit()
+		{
+			FleckMaker.Static(this.parent.DrawPos + this.Props.offset, this.parent.MapHeld, this.Props.fleck, 1f);
+		}
+
+		public override void PostExposeData()
+		{
+			base.PostExposeData();
+			Scribe_Values.Look<int>(ref this.ticksSinceLastEmitted, ((this.Props.saveKeysPrefix != null) ? (this.Props.saveKeysPrefix + "_") : "") + "ticksSinceLastEmitted", 0, false);
+		}
+		public int ticksSinceLastEmitted;
+
+	}
+}
---
+++ Source_1.3/Comps/CompProperties_ArmedEmitter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace RimDungeon
+{
+    class CompProperties_ArmedEmitter : CompProperties
+    {
+        public CompProperties_ArmedEmitter()
+        {
+            this.compClass = typeof(CompArmedEmitter);
+        }
+
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+            if (this.fleck == null)
+            {
+                yield return "CompFleckEmitter must have a fleck assigned.";
+            }
+            yield break;
+        }
+        public FleckDef fleck;
+        public Vector3 offset;
+        public int emissionInterval = -1;
+        public string saveKeysPrefix;
+    }
+}
